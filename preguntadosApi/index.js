3fcbd43089cba7cdb9fb1de22b0f6121bea185da
fannyzhl <fannyzhl583@gmail.com>
2020-11-21 15:51:19 -0400

    cambios necesarios

--- preguntadosApi/index.js
+++ preguntadosApi/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
+const dotenv = require("dotenv");}
 dotenv.config();
 const usersRoute = require("./app/routes/usersRoute");
 const leaderboardRoute = require("./app/routes/leaderboardRoute");
